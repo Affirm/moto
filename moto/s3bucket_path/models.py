1d7bcf4775d673c6dae93e8b8e627fb6e14d89f1
Greg Sterin <greg.sterin@affirm.com>
2017-04-07 15:58:00 -0700

    Fix so that it s3bucket_path server works properly

--- moto/s3bucket_path/models.py
+++ moto/s3bucket_path/models.py
@@ -1,3 +1,9 @@
 from moto.s3.models import S3Backend
 
-s3bucket_path_backend = S3Backend()
+
+# Needs to be implemented so that the urls module is mapped properly
+class S3BucketPathBackend(S3Backend):
+    def __init__(self):
+        super(S3BucketPathBackend, self).__init__()
+
+s3bucket_path_backend = S3BucketPathBackend()
--- moto/s3bucket_path/responses.py
+++ moto/s3bucket_path/responses.py
@@ -1,6 +1,7 @@
 from moto.s3.responses import ResponseObject as S3ResponseObject
 from .models import s3bucket_path_backend
 
+
 class ResponseObject(S3ResponseObject):
     def __init__(self, backend):
         super(ResponseObject, self).__init__(backend)
